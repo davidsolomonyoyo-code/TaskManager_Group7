e9d4699dced35aa12db24a4dd84032ff8a05dcef
davidsolomonyoyo-code <davidsolomonyoyo@gmail.com>
2026-05-03 07:30:20 +0300

    Renamed the files to correct file extensions

---
+++ script.js
@@ -0,0 +1,52 @@
+// DRAG & DROP 
+function renderTasks() {
+    taskList.innerHTML = '';
+
+    tasks.forEach((task, index) => {
+        const li = document.createElement('li');
+        li.className = `task-item ${task.priority}`;
+        li.setAttribute('draggable', 'true');
+        li.setAttribute('data-index', index);
+
+        li.innerHTML = `
+            <div>
+                <strong>${task.title}</strong><br>
+                <small>${task.date} | ${task.priority}</small>
+            </div>
+            <div class="actions">
+                <button onclick="editTask(${task.id})">Edit</button>
+                <button onclick="deleteTask(${task.id})" style="color:red">Delete</button>
+            </div>
+        `;
+
+        //          DRAG AND DROP EVENT LISTENERS
+        
+        //start dragging
+        li.addEventListener('dragstart', () => {
+            draggedItemIndex = index;
+            li.classList.add('dragging');
+        });
+
+        //Allow item to be dropped over this element
+
+        li.addEventListener('dragover', (e) => {
+            e.preventDefault(); // Necessary to allow drop
+        });
+
+        //handles the drop
+        li.addEventListener('drop', () => {
+            // Reorder the array based on where the item was dropped
+            const movedItem = tasks.splice(draggedItemIndex, 1)[0];
+            tasks.splice(index, 0, movedItem);
+            
+            saveAndRender(); // Save the new order permanently
+        });
+
+        //Clean up visual state
+        li.addEventListener('dragend', () => {
+            li.classList.remove('dragging');
+        });
+
+        taskList.appendChild(li);
+    });
+}
