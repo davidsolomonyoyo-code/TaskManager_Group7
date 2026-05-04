610cbc759c6fe916b75c7216e325e8a30dbcc602
StephanieKasina <stephaniekasina2@gmail.com>
2026-05-04 19:12:41 +0300

    Update script.js
    Added rendered function

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
+//===== RENDER FUNCTION
+function renderTasks() {
+  var list = document.getElementById('taskListSection');
+  list.innerHTML = '';
+  for (var i = 0; i < tasks.length; i++) {
+    var task = tasks[i];
+    var card = document.createElement('div');
+    card.className = 'task-card';
+    card.innerHTML = '<strong>' + task.title + '</strong>' +
+      '<span class="badge ' + task.priority.toLowerCase() + '">' + task.priority + '</span>' +
+      '<p>Due: ' + task.date + '</p>' +
+      '<button onclick="editTask(' + task.id + ')">Edit</button>' +
+      '<button onclick="deleteTask(' + task.id + ')">Delete</button>';
+    makeDraggable(card, i);
+    list.appendChild(card);
+  }
+}
 // DRAG & DROP 
 function renderTasks() {
     taskList.innerHTML = '';
